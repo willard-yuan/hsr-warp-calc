750f6cbd3e7850f22e1e8c222fbe72a0d31654eb
willard-yuan <yongyuanstu@gmail.com>
2025-10-07 18:25:03 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RBP94WC1RV"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RBP94WC1RV');
+          `}
+        </Script>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2934132820353961"
